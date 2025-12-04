acc928e71747ad8f6f66eb60798edc2872453c05
ASU_ziyi zhang <127939489+ASU-Ziyi-Zhang@users.noreply.github.com>
2025-12-04 14:10:14 -0700

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,67 +1,27 @@
 @echo off
-REM https://sumo.dlr.de/docs/Downloads.php
-REM https://docs.python.org/3/library/venv.html
-
-REM Check that Python and pip are installed
-SET COMMAND=python
-
-WHERE python >NUL 2>&1
-IF %ERRORLEVEL% EQU 0 SET COMMAND=python
-
-WHERE python3 >NUL 2>&1
-IF %ERRORLEVEL% EQU 0 SET COMMAND=python3
-
-WHERE py >NUL 2>&1
-IF %ERRORLEVEL% EQU 0 SET COMMAND=py
-
-IF NOT DEFINED COMMAND (
-    echo Could not find Python installed in path. Exiting.
-    exit /b 1
-)
-
-WHERE pip >NUL 2>&1
+REM Check for cmake installation
+WHERE cmake >NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo Could not find pip installed in path. Exiting.
-
+    echo cmake could not be found! Exiting controller installation. Please install/fix cmake and then try again.
+    
     pause
     exit /b 1
-) ELSE (
-    echo pip found
 )
 
-REM Install SUMO
-SET USING_VENV=0
+REM PCC
+cd pcc_codegen
+call install.bat
+call copy.bat
 
-IF %USING_VENV% EQU 1 (
-    %COMMAND% -m venv sumo_env
-    cd sumo_env
+copy _cppwrapper.py ..\..\src\pcc_cppwrapper.py
 
-    REM Activate virtual environment
-    if exist Scripts\activate.bat (
-        echo Windows detected.
-        call Scripts\activate.bat
-    ) ELSE (
-        echo Could not determine OS type.
-        echo Manually activate the virtual environment through the script Scripts\activate.bat.
-        echo Manually install sumo to virtual environment through pip:
-        echo     pip install eclipse-sumo
-        echo     pip install traci
-        echo     pip install libsumo
-        
-        pause
-        exit /b 1
-    )
+cd ..
 
-    pip install eclipse-sumo
-    pip install traci
-    pip install libsumo
-) ELSE (
-    pip install eclipse-sumo
-    pip install traci
-    pip install libsumo
-)
+REM CAV Eco-approach
+cd cavctrl_codegen
+call install.bat
+call copy.bat
 
-REM Codegen step
-cd codegen
-start .\install.bat
+copy _cwrapper.py ..\..\src\cav_cwrapper.py
+
 cd ..
--- install.sh
+++ install.sh
@@ -1,90 +1,27 @@
 #!/bin/sh
 
-## https://sumo.dlr.de/docs/Downloads.php
-## https://docs.python.org/3/library/venv.html
-
-## Check that python and pip are installed 
-COMMAND=python # Default command to try
-if [ -x "$(command -v python)" ]; then
-    COMMAND=python
-
-elif [ -x "$(command -v python3)" ]; then
-    COMMAND=python3
-
-elif [ -x "$(command -v py)" ]; then
-    COMMAND=py
-
-else
-    echo "Could not find Python installed in path. Exiting."
-    exit 1
-
-fi
-
-# PIP
-if [ -x "$(command -v pip)" ]; then
-    echo "pip found"
-
-else
-    echo "Could not find pip installed in path. Exiting."
-    exit 1
-    
-fi
-
-## Install SUMO
-# Settings
-USING_VENV=0 # 1 to install to virtual environment, 0 to install sumo globally
-
-# Install
-if [ $USING_VENV -eq 1 ]; then
-    $COMMAND -m venv sumo_env
-    cd sumo_env
-    
-    # Run the activate script
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        echo "Linux GNU detected."
-        . bin/activate
-
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "Mac OSX detected."
-        . bin/activate
-
-    elif [[ "$OSTYPE" == "cygwin" ]]; then
-        echo "POSIX/Linux for Windows detected."
-        Scripts\Activate.ps1
-
-    else
-        echo "Could not determine OS type. "
-        echo "Manually activate the virutal environment through script bin/activate."
-        echo "Manually install sumo to virtual environemtn through pip:"
-        echo "    pip install eclipse-sumo"
-        echo "    pip install traci"
-        echo "    pip install libsumo"
-        
-        exit 1
-    fi
-    
-    pip install eclipse-sumo
-    pip install traci
-    pip install libsumo
-
-    pip install matplotlib
-    pip install numpy
-    pip install pandas
-    pip install scipy
+## Check for cmake install
+if ! command -v cmake 2>&1 >/dev/null
+then
+    echo "cmake could not be found! Exiting controller installation. Please install/fix cmake and then try again."
     
 else
-    pip install eclipse-sumo
-    pip install traci
-    pip install libsumo
+    ## PCC
+    cd pcc_codegen
+    . install.sh
+    . copy.sh
 
-    pip install matplotlib
-    pip install numpy
-    pip install pandas
-    pip install scipy
+    cp _cppwrapper.py ../../src/pcc_cppwrapper.py
+
+    cd ..
+
+    ## CAV Eco-approach
+    cd cavctrl_codegen
+    . install.sh
+    . copy.sh
+
+    cp _cwrapper.py ../../src/cav_cwrapper.py
+
+    cd ..
 
 fi
-
-## Codegen
-cd codegen
-. install.sh
-cd ..
