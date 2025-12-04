92fb7e1f21428b937155fc0dcf9da9ae4bd39386
ASU_ziyi zhang <127939489+ASU-Ziyi-Zhang@users.noreply.github.com>
2025-12-04 14:21:03 -0700

    Add files via upload

---
+++ codegen/cavtrl_codegen/copy.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Check filename
+SET DEST=..\..\
+
+REM Default file
+SET FILE=libcav_so.so
+
+REM Detect OS type
+ver | findstr /i "Windows" >NUL
+IF %ERRORLEVEL% EQU 0 (
+    echo Windows detected.
+    SET FILE=cav_so.dll
+) ELSE (
+    echo Non-Windows OS detected.
+    SET FILE=libcav_so.so
+)
+
+REM Copy the file to specified directories
+echo Copying file %FILE% to %DEST%
+copy lib\%FILE% %DEST%
+
+REM Check if copy was successful
+IF %ERRORLEVEL% NEQ 0 (
+    echo Copy Code: %ERRORLEVEL% - Unsuccessful
+)
